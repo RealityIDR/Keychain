50a8e088ea3da6485d00a3afb66d6c68e763f1ef
Isaac <realityidr.529@gmail.com>
2022-10-19 13:54:10 -0700

    Changes to show Navigation

--- components/RightScreen/index.js
+++ components/RightScreen/index.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 import MapView from 'react-native-maps';
+import Navigation from '../Navigation';
 
 const RightScreen = (props) => {
+    
+    const [app, setApp] = useState(<Navigation />);
+ 
     return (
         <View style={styles.container}>
-            <MapView
-                style={styles.application}
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-            />
+            {app}
             <View style={styles.widget}></View>
         </View>
     );
